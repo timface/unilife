c8ab6fe0da8aa900ecd8139f599daf70ff36b307
timface <timothy.hart@my.jcu.edu.au>
2018-03-06 09:43:03 +1000

    Started implementing room class

---
+++ UniLife/Assets/Model/Room.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum RoomType { OFFICE, LECTURE, PRAC, FOOD, DORM }
+
+public class Room { 
+
+    public List<Tile> tiles
+    {
+        get;
+        protected set;
+    }
+
+    public RoomType roomType;
+
+    public List<Fixture> requiredFixtures;
+
+    public bool IsComplete
+    {
+        get;
+        protected set;
+    }
+
+    int roomIndex;
+}
